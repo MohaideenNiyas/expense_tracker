f66de88b82b5aa529804a0278e03ebc5d4876dc7
MohaideenNiyas <mohaideenniyas04@gmail.com>
2025-08-17 22:48:41 +0530

    Update client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { auth } from "../auth/firebase";
 import { onIdTokenChanged, getIdToken } from "firebase/auth";
-
+console.log("API URL from env:", import.meta.env.VITE_API_URL);
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   timeout: 15000
